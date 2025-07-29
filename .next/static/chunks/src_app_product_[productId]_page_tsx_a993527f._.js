5129741878c516d5cddf25ad6c81a346e471aed8
Akik2003 <ghostavg33@gmail.com>
2025-07-29 16:03:06 +0530

    Add .next/static/chunks/src_app_product_[productId]_page_tsx_a993527f._.js 1753785185473

---
+++ .next/static/chunks/src_app_product_[productId]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_d0f09b98._.js",
+  "static/chunks/src_700001ba._.js"
+],
+    source: "dynamic"
+});
